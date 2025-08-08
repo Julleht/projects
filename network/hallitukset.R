df5c72a63a09cb101da48e1bb2754496ab433778
Julius Lehtinen <32741853+Julleht@users.noreply.github.com>
2025-08-08 12:08:03 +0300

    Update hallitukset.R

--- network/hallitukset.R
+++ network/hallitukset.R
@@ -49,7 +49,7 @@ interwar <- ggraph(netw1, layout = "fr", maxiter=100000000)+
   scale_edge_width_continuous(range = c(0,3), limits = c(0,max(df1$weight)))+
   scale_edge_alpha_continuous(range = c(0.1,1), limits = c(0,max(df1$weight)))+
   scale_fill_manual(values = colours)+
-  labs(title="Suomalaisten puolueiden hallituskumppanit rauhan aikana 1918–1939",
+  labs(title="Suomalaisten puolueiden hallituskumppanit 1918–1939",
        subtitle = "Mitä paksumpi ja näkyvämpi viiva puolueiden välillä,\nsitä useamman päivän samassa hallituksessa",
        caption = "Julius Lehtinen / Databyro"
   )+
@@ -63,7 +63,7 @@ interwar <- ggraph(netw1, layout = "fr", maxiter=100000000)+
     plot.margin = margin(10,10,10,10)
   )
 
-postwar <- ggraph(netw2, layout = "fr", maxiter=10000000)+
+postwar <- ggraph(netw2, layout = "fr", maxiter=100000)+
   geom_edge_link(aes(width=weight, alpha=weight, start_cap = label_rect(node1.name),
                      end_cap = label_rect(node2.name)))+
   geom_node_label(aes(label=name, fill=name), colour="white", size=6, label.padding = unit(0.45, "lines"))+
@@ -71,7 +71,7 @@ postwar <- ggraph(netw2, layout = "fr", maxiter=10000000)+
   scale_edge_width_continuous(range = c(0,3), limits = c(0,max(df2$weight)))+
   scale_edge_alpha_continuous(range = c(0.1,1), limits = c(0,max(df2$weight)))+
   scale_fill_manual(values = colours)+
-  labs(title="Suomalaisten puolueiden hallituskumppanit rauhan aikana 1945–1991",
+  labs(title="Suomalaisten puolueiden hallituskumppanit 1945–1991",
        subtitle = "Mitä paksumpi ja näkyvämpi viiva puolueiden välillä,\nsitä useamman päivän samassa hallituksessa",
        caption = "Julius Lehtinen / Databyro"
   )+
@@ -85,7 +85,7 @@ postwar <- ggraph(netw2, layout = "fr", maxiter=10000000)+
     plot.margin = margin(10,10,10,10)
   )
 
-coop <- ggraph(netw3, layout = "fr", maxiter=10000000)+
+coop <- ggraph(netw3, layout = "fr", maxiter=1000000)+
   geom_edge_link(aes(width=weight, alpha=weight, start_cap = label_rect(node1.name),
                      end_cap = label_rect(node2.name)))+
   geom_node_label(aes(label=name, fill=name), colour="white", size=6, label.padding = unit(0.45, "lines"))+
@@ -93,7 +93,30 @@ coop <- ggraph(netw3, layout = "fr", maxiter=10000000)+
   scale_edge_width_continuous(range = c(0,3), limits = c(0,max(df3$weight)))+
   scale_edge_alpha_continuous(range = c(0.1,1), limits = c(0,max(df3$weight)))+
   scale_fill_manual(values = colours)+
-  labs(title="Suomalaisten puolueiden hallituskumppanit rauhan aikana 1991–2022",
+  labs(title="Suomalaisten puolueiden hallituskumppanit 1991–2025",
+       subtitle = "Mitä paksumpi ja näkyvämpi viiva puolueiden välillä,\nsitä useamman päivän samassa hallituksessa",
+       caption = "Julius Lehtinen / Databyro"
+  )+
+  coord_cartesian(clip="off")+
+  theme_graph()+
+  theme(
+    #panel.background = element_rect(colour="black"),
+    legend.position = "none",
+    plot.title = element_text(size=32, face = "bold"),
+    plot.subtitle = element_text(size=26),
+    plot.margin = margin(10,10,10,10)
+  )
+coop
+
+total <- ggraph(netw4, layout = "fr", maxiter=1000000)+
+  geom_edge_link(aes(width=weight, alpha=weight, start_cap = label_rect(node1.name),
+                     end_cap = label_rect(node2.name)))+
+  geom_node_label(aes(label=name, fill=name), colour="white", size=6, label.padding = unit(0.45, "lines"))+
+  geom_shadowtext(aes(x, y, label=name), size=6)+
+  scale_edge_width_continuous(range = c(0.1,3), limits = c(0,max(df4$weight)))+
+  scale_edge_alpha_continuous(range = c(0.1,1), limits = c(0,max(df4$weight)))+
+  scale_fill_manual(values = colours)+
+  labs(title="Suomalaisten puolueiden hallituskumppanit rauhan aikana 1918–2025",
        subtitle = "Mitä paksumpi ja näkyvämpi viiva puolueiden välillä,\nsitä useamman päivän samassa hallituksessa",
        caption = "Julius Lehtinen / Databyro"
   )+
@@ -107,32 +130,10 @@ coop <- ggraph(netw3, layout = "fr", maxiter=10000000)+
     plot.margin = margin(10,10,10,10)
   )
 
-total <- ggraph(netw4, layout = "fr", maxiter=100000000)+
-  geom_edge_link(aes(width=weight, alpha=weight, start_cap = label_rect(node1.name),
-                     end_cap = label_rect(node2.name)))+
-  geom_node_label(aes(label=name, fill=name), colour="white", size=6, label.padding = unit(0.45, "lines"))+
-  geom_shadowtext(aes(x, y, label=name), size=6)+
-  scale_edge_width_continuous(range = c(0.1,3), limits = c(0,max(df4$weight)))+
-  scale_edge_alpha_continuous(range = c(0.1,1), limits = c(0,max(df4$weight)))+
-  scale_fill_manual(values = colours)+
-  labs(title="Suomalaisten puolueiden hallituskumppanit rauhan aikana 1918–2022",
-       subtitle = "Mitä paksumpi ja näkyvämpi viiva puolueiden välillä,\nsitä useamman päivän samassa hallituksessa",
-       caption = "Julius Lehtinen / Databyro"
-       )+
-  coord_cartesian(clip="off")+
-  theme_graph()+
-  theme(
-    #panel.background = element_rect(colour="black"),
-    legend.position = "none",
-    plot.title = element_text(size=32, face = "bold"),
-    plot.subtitle = element_text(size=26),
-    plot.margin = margin(10,10,10,10)
-  )
-
-ggsave("network_interwar.png", dpi = 1000, plot = interwar, height = 225, width = 400, units = "mm")
-ggsave("network_postwar.png", dpi = 1000, plot = postwar, height = 225, width = 400, units = "mm")
-ggsave("network_coop.png", dpi = 1000, plot = coop, height = 225, width = 400, units = "mm")
-ggsave("network_total.png", dpi = 1000, plot = total, height = 225, width = 400, units = "mm")
+ggsave("network_interwar.png", dpi = 400, plot = interwar, height = 225, width = 400, units = "mm")
+ggsave("network_postwar.png", dpi = 400, plot = postwar, height = 225, width = 400, units = "mm")
+ggsave("network_coop.png", dpi = 400, plot = coop, height = 225, width = 400, units = "mm")
+ggsave("network_total.png", dpi = 400, plot = total, height = 225, width = 400, units = "mm")
 
 (interwar + postwar) / (coop + total) +
   plot_annotation(
